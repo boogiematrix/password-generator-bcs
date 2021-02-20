1ad555fa94f986d265615020d7aa7b954d41af27
Night Chough <chicken.snuggles@gmail.com>
2021-02-20 17:13:14 -0600

    added psuedocode, password length prompt, random character generator

--- script.js
+++ script.js
@@ -14,12 +14,25 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page*/
 
+//An object containing arrays of all possible characters in the password
 const characterTypes = {
-  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
   number: [0,1, 2, 3, 4, 5, 6, 7, 8, 9],
-  special: [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
+  special: [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.',
+   '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
 }
+/* prompt window- input must be a number. must be between 8 and 128 'How many characters in your password?'
+if not a number 'You must enter a number between 8 and 128*/
+
+/* window.confirm for each character type. must be nested in number prompt. and each type confirm nested
+in the previous one. maybe all in a do while loop */ 
+
+/*IF user selects only one character category {Math.floor(Math.random(categoryX.length))}
+otherwise use  category1.concat(category2.concat(category3.concat(category4) and then 
+random number method on resulting array*/
 
 
 // Assignment Code
@@ -36,3 +49,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  do {
+  var passLength = window.prompt('Choose a length for your password between 8 and 128');
+  } while (passLength < 8 || passLength > 128) 
+  
+
+  do {
+    window.confirm
+  } while (passLength < 8 || passLength > 128 || finalPasswordArray == null) 
+  
+}
+
+
+// Adding the actual random character to the password string
+for (i = 0; i <= passLength ; i ++) {
+  password += finalPasswordArray[Math.floor(Math.random()*finalPasswordArray.length)]
+}
