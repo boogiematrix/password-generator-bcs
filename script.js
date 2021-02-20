916c61861a5c53937b0c5e18afb6cc80fbe59ec8
Night Chough <chicken.snuggles@gmail.com>
2021-02-20 15:42:07 -0600

    added an object containing arrays of all possible password characters

--- script.js
+++ script.js
@@ -14,6 +14,14 @@ THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page*/
 
+const characterTypes = {
+  lowerCase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperCase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  number: [0,1, 2, 3, 4, 5, 6, 7, 8, 9],
+  special: [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'],
+}
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
